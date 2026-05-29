f9f8a911e19a51382f927de6ae2b3961d7885214
alpsolucoesdigitais <alpsolucoesdigitais@gmail.com>
2026-05-28 22:46:20 -0300

    Delete src directory

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import { VidstackPlayer, PlyrLayout } from "vidstack/global/player";
-
-export { VidstackPlayer, PlyrLayout };
-
-import "vidstack/player/styles/base.css";
-import "vidstack/player/styles/plyr/theme.css";
