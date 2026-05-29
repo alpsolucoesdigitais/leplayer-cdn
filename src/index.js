c40493d4cf3893d5c094d16b915950d31ea7f151
alpsolucoesdigitais <alpsolucoesdigitais@gmail.com>
2026-05-28 22:28:30 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { VidstackPlayer, PlyrLayout } from "vidstack/global/player";
+
+export { VidstackPlayer, PlyrLayout };
+
+import "vidstack/player/styles/base.css";
+import "vidstack/player/styles/plyr/theme.css";
